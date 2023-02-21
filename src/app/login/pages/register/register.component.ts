51904fa5811696c81310c272aae31f6c428429c8
Cupcea Petru <petru.cupcea@getik.net>
2023-02-22 00:49:23 +0200

    hashing before inserting a new user works

--- src/app/login/pages/register/register.component.ts
+++ src/app/login/pages/register/register.component.ts
@@ -1,17 +1,26 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, UntypedFormGroup, Validators } from '@angular/forms';
 
+import { ApiRequestService } from '../../../api-module';
+import { BaseComponent } from '../../../shared';
+import { takeUntil } from 'rxjs';
+
+
 @Component({
   selector: 'register-page',
   templateUrl: 'register.component.html',
   styleUrls: ['register.component.scss'],
 })
-export class RegisterComponent {
+export class RegisterComponent extends BaseComponent {
 
   formGroup: UntypedFormGroup;
 
 
-  constructor(formBuilder: FormBuilder) {
+  constructor(
+    private readonly apiRequestService: ApiRequestService,
+    formBuilder: FormBuilder,
+  ) {
+    super();
     this.formGroup = formBuilder.group({
       name: new FormControl('', [Validators.required]),
       surname: new FormControl('', [Validators.required]),
@@ -20,6 +29,27 @@ export class RegisterComponent {
       password: new FormControl('', [Validators.required]),
       repeatPassword: new FormControl('', [Validators.required])
     })
+  }
+
+
+  registerNewUser() {
+    if (this.formGroup.invalid) {
+      return;
+    }
+    this.apiRequestService.callOperation('create_user', {...this.formGroup.getRawValue()})
+      .pipe(takeUntil(this.onDestroy))
+      .subscribe(() => {
+        this.getUsers();
+      })
+  }
+
+
+  getUsers() {
+    this.apiRequestService.callOperation('get_users')
+      .pipe(takeUntil(this.onDestroy))
+      .subscribe((data) => {
+        console.log(data.payload);
+      })
   }
 
 }
